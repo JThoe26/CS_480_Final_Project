7e932fed254274ece3b4d82b8b6944073e40fe07
JThoe26 <jackson.thoe@mnsu.edu>
2024-12-12 15:58:53 -0600

    login example

---
+++ login_microservice/login/login_app.py
@@ -0,0 +1,23 @@
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+# In-memory user storage for demonstration purposes
+users = {
+    "user1": "password1",
+    "user2": "password2"
+}
+
+@app.route('/login', methods=['POST'])
+def login():
+    data = request.get_json()
+    username = data.get('username')
+    password = data.get('password')
+
+    if username in users and users[username] == password:
+        return jsonify({"message": "Login successful"}), 200
+    else:
+        return jsonify({"message": "Invalid username or password"}), 401
+
+if __name__ == '__main__':
+    app.run(debug=True)
